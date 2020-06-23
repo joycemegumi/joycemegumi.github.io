1af240f5d331afdc77f7b8a64a17f37ca8981a4f
joycemegumi <65522775+joycemegumi@users.noreply.github.com>
2020-06-23 01:20:36 -0400

    added moving-letters#11

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,32 @@
-document.querySelector('.js-btn').addEventListener('click', function() {
-    console.log('PRESSED');
-    const heading = document.querySelector('#my-id');
+const letters = document.querySelector('.letters');
 
-    if (heading) {
-        heading.innerHTML += ' Joyce';
-    }
-});
+  ml.timelines["ml11"] = anime.timeline({loop: false})
+    .add({
+      targets: '.ml11 .line',
+      scaleY: [0,1],
+      opacity: [0.5,1],
+      easing: "easeOutExpo",
+      duration: 700
+    })
+    .add({
+      targets: '.ml11 .line',
+      translateX: [0,letters.offsetWidth],
+      easing: "easeOutExpo",
+      duration: 700,
+      delay: 100
+    }).add({
+      targets: '.ml11 .letter',
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=775',
+      delay: function(el, i) {
+        return 34 * (i+1)
+      }
+    }).add({
+        targets: '.ml11 .line',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 500
+    });
